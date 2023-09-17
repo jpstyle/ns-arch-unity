d5394c72e78d001b993133c3cc7ad858216d0cbc
jpstyle <pjh4g92@gmail.com>
2023-09-17 21:42:54 +0100

    Exp1 replication summarization script done

--- tools/exp1_summ.py
+++ tools/exp1_summ.py
@@ -8,7 +8,6 @@ sys.path.insert(
     0,
     os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 )
-import re
 import csv
 import uuid
 import logging
@@ -47,10 +46,12 @@ def main(cfg):
                 dirs_with_results.append((date_dir, time_dir))
 
     results_cumulReg = {}
-    # results_confMat = defaultdict(lambda: defaultdict(dict))
-    # results_learningCurve = defaultdict(dict)
+    results_learningCurve = defaultdict(lambda: defaultdict(list))
+    results_confMat = defaultdict(
+        lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    )               # This is one ugly nesting but bear with me, myself...
 
-    num_concepts = {}
+    all_seeds = set()
 
     # Collect results
     for res_dir in tqdm.tqdm(dirs_with_results, total=len(dirs_with_results)):
@@ -58,9 +59,12 @@ def main(cfg):
         dir_contents = list(os.listdir(res_dir))
 
         for data in dir_contents:
-            name_parse = re.findall(r"(.+)_(.+)_(.+)_(\d+)", data)[0]
-            data_type, *exp_config, seed = name_parse
-            strat_fb, strat_gn = exp_config
+            name_parse = data.split(".")[0].split("_")
+            data_type = name_parse[0]
+            strat_fb, strat_gn = name_parse[1], name_parse[2]
+            seed = name_parse[3]
+
+            all_seeds.add(seed)
 
             if data_type == "cumulReg":
                 # Cumulative regret curve data
@@ -70,8 +74,9 @@ def main(cfg):
                     # Header column; 'episode,cumulative_regret'
                     _ = next(reader)
 
-                    curve = np.array([[int(row[0]), int(row[1])] for row in reader])
+                    curve = np.array([[int(ep_num), int(regret)] for ep_num, regret in reader])
 
+                    # Aggregate cumulative regret curve data
                     if (strat_fb, strat_gn) in results_cumulReg:
                         stats_agg = results_cumulReg[(strat_fb, strat_gn)]
                         for ep_num, regret in curve:
@@ -84,47 +89,43 @@ def main(cfg):
                             ep_num: [regret] for ep_num, regret in curve
                         }
 
-            elif data_type.startswith("confMat"):
-                # Confusion matrix data
-                num_exs = int(data_type.strip("confMat"))
-
+            elif data_type == "outputs":
+                # Test question-answer pairs, from which learning curves and confusion
+                # matrices can be plotted
+                num_exs = int(name_parse[-1])
                 with open(os.path.join(res_dir, data)) as data_f:
                     reader = csv.reader(data_f)
-                    concepts = next(reader)
 
-                    confMat = np.array([[float(d) for d in row] for row in reader])
-
-                    if (strat_fb, strat_gn) in results_confMat[diff][num_exs]:
-                        stats_agg = results_confMat[diff][num_exs][(strat_fb, strat_gn)]
-                        stats_agg["matrix"].append(confMat)
-                        stats_agg["num_test_suites"] += 1
-                        assert stats_agg["concepts"] == concepts
-                    else:
-                        results_confMat[diff][num_exs][(strat_fb, strat_gn)] = {
-                            "matrix": [confMat],
-                            "num_test_suites": 1,
-                            "concepts": concepts
-                        }
-
-            elif data_type.startswith("mAPs"):
-                # Learning curve data (performance measured by mAP)
-                with open(os.path.join(res_dir, data)) as data_f:
-                    reader = csv.reader(data_f)
+                    # Header column; 'episode,ground_truth,answer'
                     _ = next(reader)
 
-                    curve = [(int(row[0]), float(row[1])) for row in reader]
+                    # Summarize into confusion matrix, then reduce into accuracy
+                    confusion_matrix = defaultdict(lambda: defaultdict(int))
+                    for _, gt_conc, ans_conc in reader:
+                        confusion_matrix[gt_conc][ans_conc] += 1
+                        confusion_matrix[gt_conc]["total"] += 1
 
-                    if (strat_fb, strat_gn) in results_learningCurve[diff]:
-                        stats_agg = results_learningCurve[diff][(strat_fb, strat_gn)]
-                        for num_exs, mAP in curve:
-                            if num_exs in stats_agg:
-                                stats_agg[num_exs].append(mAP)
-                            else:
-                                stats_agg[i] = [mAP]
-                    else:
-                        results_learningCurve[diff][(strat_fb, strat_gn)] = {
-                            num_exs: [mAP] for num_exs, mAP in curve
+                    all_concs = list(confusion_matrix)      # Collect and assign order
+
+                    confusion_matrix = {
+                        gt_conc: {
+                            conc: answers[conc] / answers["total"] if conc in answers else 0
+                            for conc in all_concs
                         }
+                        for gt_conc, answers in confusion_matrix.items()
+                    }
+                    accuracy = sum(
+                        answers[gt_conc] for gt_conc, answers in confusion_matrix.items()
+                    ) / len(confusion_matrix)           # Mean accuracy across ground truths
+
+                    # Aggregate learning curve data
+                    results_learningCurve[(strat_fb, strat_gn)][num_exs].append(accuracy)
+
+                    # Aggregate confusion matrix data
+                    for gt_conc, answers in confusion_matrix.items():
+                        for ans_conc, ratio in answers.items():
+                            matrix_slot = results_confMat[(strat_fb, strat_gn)][gt_conc][ans_conc]
+                            matrix_slot[num_exs].append(ratio)
 
             else:
                 continue
@@ -154,8 +155,7 @@ def main(cfg):
     _, ax = plt.subplots()
     ymax = 0
 
-    for exp_config, data in results_cumulReg.items():
-        strat_fb, strat_gn = exp_config
+    for (strat_fb, strat_gn), data in results_cumulReg.items():
         stats = [
             (i, np.mean(rgs), 1.96 * np.std(rgs)/np.sqrt(len(rgs)))
             for i, rgs in data.items()
@@ -193,107 +193,114 @@ def main(cfg):
     labels = [config_aliases.get(hl[1], hl[1]) for hl in hls_sorted]
     ax.legend(handles, labels)
     
-    ax.set_title(f"Cumulative regret curve (N={len(data[0])} per config)")
+    ax.set_title(f"Cumulative regret curve (N={len(all_seeds)} per config)")
     plt.savefig(os.path.join(cfg.paths.outputs_dir, f"cumulReg.png"))
 
-    # # Aggregate and visualize: confusion matrices
-    # for diff, per_diff in results_confMat.items():
-    #     last_num_exs = max(per_diff)
-    #     for num_exs, per_config in per_diff.items():
-    #         for exp_config, data in per_config.items():
-    #             strat_fb, strat_gn = exp_config
-    #             config_label = f"{strat_gn}_{strat_fb}"
+    # Aggregate and visualize: learning curve
+    _, ax = plt.subplots()
 
-    #             num_concepts[diff] = len(data["matrix"][0])
+    for (strat_fb, strat_gn), data in results_learningCurve.items():
+        data = sorted([entry for entry in data.items()], key=lambda x: x[0])
+        stats = [
+            (num_exs, np.mean(accs), 1.96 * np.std(accs)/np.sqrt(len(accs)))
+            for num_exs, accs in data
+        ]
 
-    #             if num_exs == last_num_exs:
-    #                 # Draw confusion matrix
-    #                 fig = plt.figure()
-    #                 draw_matrix(
-    #                     sum(data["matrix"]) / data["num_test_suites"],
-    #                     data["concepts"], data["concepts"], fig.gca()    # Binary choice mode
-    #                 )
-    #                 plt.suptitle(f"Confusion matrix for ({diff}; N={len(data['matrix'])} per config)", fontsize=16)
-    #                 plt.title(f"{config_label} agent", pad=18)
-    #                 plt.tight_layout()
-    #                 plt.savefig(os.path.join(cfg.paths.outputs_dir, f"confMat_{diff}_{config_label}.png"))
+        # Plot mean curve
+        ax.plot(
+            [num_exs for num_exs, _, _ in stats],
+            [mmAP for _, mmAP, _ in stats],
+            label=f"{strat_gn}_{strat_fb}",
+            color=config_colors[f"{strat_gn}_{strat_fb}"]
+        )
+        ax.plot(
+            [0, stats[0][0]], [0, stats[0][1]],
+            color=config_colors[f"{strat_gn}_{strat_fb}"], linestyle="dashed"
+        )
+        # Plot confidence intervals
+        ax.fill_between(
+            [0]+[num_exs for num_exs, _, _ in stats],
+            [0]+[mmAP-cl for _, mmAP, cl in stats],
+            [0]+[mmAP+cl for _, mmAP, cl in stats],
+            color=config_colors[f"{strat_gn}_{strat_fb}"], alpha=0.2
+        )
 
-    # # Aggregate and visualize: learning curve
-    # for diff, agg_stats in results_learningCurve.items():
-    #     fig, ax = plt.subplots()
+    # Plot curve
+    ax.set_xlabel("# training examples")
+    ax.set_ylabel("mAP score")
+    ax.set_xlim(0, stats[-1][0])
+    ax.set_ylim(0, 1)
+    ax.grid()
 
-    #     for exp_config, data in agg_stats.items():
-    #         strat_fb, strat_gn = exp_config
-    #         data = sorted(data.items())
-    #         stats = [
-    #             (num_exs, np.mean(mAPs), 1.96 * np.std(mAPs)/np.sqrt(len(mAPs)))
-    #             for num_exs, mAPs in data
-    #         ]
+    # Ordering legends according to the prespecified ordering above
+    handles, labels = ax.get_legend_handles_labels()
+    hls_sorted = sorted(
+        [(h, l) for h, l in zip(handles, labels)],
+        key=lambda x: config_ord.index(x[1])
+    )
+    handles = [hl[0] for hl in hls_sorted]
+    labels = [config_aliases.get(hl[1], hl[1]) for hl in hls_sorted]
+    plt.legend(handles, labels)
+    
+    plt.title(f"Learning curve (N={len(all_seeds)} per config)")
+    plt.savefig(os.path.join(cfg.paths.outputs_dir, f"learningCurve.png"))
 
-    #         # Temporary adjustment
-    #         if diff=="fineEasy":
-    #             stats = [s for s in stats if s[0] <= 30]
+    # Aggregate and visualize: confusion matrices
+    fig = plt.figure(figsize=(8, 6), dpi=80)
+    gs = fig.add_gridspec(len(all_concs), len(all_concs), hspace=0, wspace=0)
+    axs = gs.subplots(sharex='col', sharey='row')
 
-    #         # Plot mean curve
-    #         ax.plot(
-    #             [num_exs for num_exs, _, _ in stats],
-    #             [mmAP for _, mmAP, _ in stats],
-    #             label=f"{strat_gn}_{strat_fb}",
-    #             color=config_colors[f"{strat_gn}_{strat_fb}"]
-    #         )
-    #         ax.plot(
-    #             [0, stats[0][0]], [1/num_concepts[diff], stats[0][1]],
-    #             color=config_colors[f"{strat_gn}_{strat_fb}"], linestyle="dashed"
-    #         )
-    #         # Plot confidence intervals
-    #         ax.fill_between(
-    #             [0]+[num_exs for num_exs, _, _ in stats],
-    #             [1/num_concepts[diff]]+[mmAP-cl for _, mmAP, cl in stats],
-    #             [1/num_concepts[diff]]+[mmAP+cl for _, mmAP, cl in stats],
-    #             color=config_colors[f"{strat_gn}_{strat_fb}"], alpha=0.2
-    #         )
+    for (strat_fb, strat_gn), data in results_confMat.items():
+        # Draw a confusion matrix, with curve plots as matrix entries (instead of single
+        # numbers at the last)
+        for gt_conc, per_gt_conc in data.items():
+            for ans_conc, stats in per_gt_conc.items():
+                # Slot to fill with curve
+                i = all_concs.index(gt_conc)
+                j = all_concs.index(ans_conc)
+                ax = axs[i][j]
 
-    #     # Plot curve
-    #     ax.set_xlabel("# training examples")
-    #     ax.set_ylabel("mAP score")
-    #     ax.set_xlim(0, stats[-1][0])
-    #     ax.set_ylim(0, 1)
-    #     ax.grid()
+                # Set tick parameters, limits, etc.
+                ax.tick_params(left=False, right=True, labelleft=False, labelright=True)
+                ax.set_xlabel(ans_conc)
+                ax.set_ylabel(gt_conc)
+                ax.set_ylim(0, 1)
+                ax.xaxis.set_label_position('top')
+                ax.set_yticks([0.25, 0.5, 0.75, 1])
 
-    #     # Ordering legends according to the prespecified ordering above
-    #     handles, labels = ax.get_legend_handles_labels()
-    #     hls_sorted = sorted(
-    #         [(h, l) for h, l in zip(handles, labels)],
-    #         key=lambda x: config_ord.index(x[1])
-    #     )
-    #     handles = [hl[0] for hl in hls_sorted]
-    #     labels = [config_aliases.get(hl[1], hl[1]) for hl in hls_sorted]
-    #     plt.legend(handles, labels)
-        
-    #     plt.title(f"Learning curve ({diff}; N={len(data[0][1])} per config)")
-    #     plt.savefig(os.path.join(cfg.paths.outputs_dir, f"learningCurve_{diff}.png"))
+                # Collect stats
+                stats = sorted([entry for entry in stats.items()], key=lambda x: x[0])
+                stats = [
+                    (num_exs, np.mean(ratios), 1.96 * np.std(ratios)/np.sqrt(len(ratios)))
+                    for num_exs, ratios in stats
+                ]
 
-    # # Report final mAP scores on terminal
-    # for diff, agg_stats in results_learningCurve.items():
-    #     first_num_exs = sorted(list(agg_stats.values())[0])[0]
-    #     middle_num_exs = sorted(list(agg_stats.values())[0])[2]
-    #     last_num_exs = sorted(list(agg_stats.values())[0], reverse=True)[0]
+                # Plot mean curve
+                ax.plot(
+                    [num_exs for num_exs, _, _ in stats],
+                    [mmAP for _, mmAP, _ in stats],
+                    label=f"{strat_gn}_{strat_fb}",
+                    color=config_colors[f"{strat_gn}_{strat_fb}"]
+                )
+                ax.plot(
+                    [0, stats[0][0]], [0, stats[0][1]],
+                    color=config_colors[f"{strat_gn}_{strat_fb}"], linestyle="dashed"
+                )
+                # Plot confidence intervals
+                ax.fill_between(
+                    [0]+[num_exs for num_exs, _, _ in stats],
+                    [0]+[mmAP-cl for _, mmAP, cl in stats],
+                    [0]+[mmAP+cl for _, mmAP, cl in stats],
+                    color=config_colors[f"{strat_gn}_{strat_fb}"], alpha=0.2
+                )
 
-    #     # Temporary adjustment
-    #     if diff=="fineEasy":
-    #         last_num_exs = 30
-
-    #     for num_exs in [first_num_exs, middle_num_exs, last_num_exs]:
-    #         print("")
-    #         logger.info(f"mAP scores after {num_exs} examples ({diff}):")
-
-    #         final_mAPs = {
-    #             f"{strat_gn}_{strat_fb}": data[num_exs]
-    #             for (strat_fb, strat_gn), data in agg_stats.items()
-    #         }
-    #         for cfg in sorted(final_mAPs, key=lambda x: config_ord.index(x)):
-    #             logger.info("\t"+f"{config_aliases.get(cfg, cfg)}: {float(np.mean(final_mAPs[cfg]))}")
-
+    for ax in fig.get_axes():
+        ax.label_outer()
+    
+    fig.suptitle(f"Confusion plot matrix for (N={len(all_seeds)} per config)", fontsize=16)
+    fig.supxlabel("# training examples")
+    fig.supylabel("Response rate")
+    plt.savefig(os.path.join(cfg.paths.outputs_dir, f"confMat.png"))
 
 if __name__ == "__main__":
     main()
